6843f37521809839c976560638b0f5c7b3881f5f
Eric Fern <fern.eric@proton.me>
2025-05-27 18:34:36 -0300

    replace helix with neovim

--- apps/notebook.sh
+++ apps/notebook.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-alacritty --class notebook -e /bin/helix "$HOME/notes" &
+alacritty --class notebook -e nvim -c ":cd $HOME/notes" -c ":Telescope find_files" -c ":highlight @neorg.headings.2.title guifg=#5DFF88" -c ":highlight @neorg.headings.2.prefix guifg=#5DFF88" &
