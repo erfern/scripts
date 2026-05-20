223df73ce8cbbfd267b06770b91353fee522557b
hireki <hireki@proton.me>
2026-05-20 12:03:29 -0300

    [refactor]: make func variables local and fix identation

--- toggle-pin-player.sh
+++ toggle-pin-player.sh
@@ -2,7 +2,7 @@
 
 get_mpv_window_size() {
     hyprctl -j clients | jq --arg addr "$address" \
-      '.[] | select(.address == $addr) | .size'
+        '.[] | select(.address == $addr) | .size'
 }
 
 get_firefox_window_size() {
@@ -10,9 +10,9 @@ get_firefox_window_size() {
 }
 
 declare -A window_size_map=(
-  [firefox]=get_firefox_window_size
-  [mpv]=get_mpv_window_size
-  )
+    [firefox]=get_firefox_window_size
+    [mpv]=get_mpv_window_size
+)
 
 pin() {
     local address="$1"
@@ -20,22 +20,28 @@ pin() {
 
     hyprctl dispatch togglefloating address:$address
 
-    MONITOR=$(hyprctl -j monitors | jq -c ".[]")
-    MONITOR_WIDTH=$(jq ".width" <<< "$MONITOR")
-    MONITOR_HEIGHT=$(jq ".height" <<< "$MONITOR")
+    local MONITOR=$(hyprctl -j monitors | jq -c ".[]")
+    local MONITOR_WIDTH=$(jq ".width" <<<"$MONITOR")
+    local MONITOR_HEIGHT=$(jq ".height" <<<"$MONITOR")
 
-    GAPS_OUT=$(hyprctl getoption general:gaps_out -j | jq -r ".custom" | cut -d ' ' -f 1)
-    BORDER_SIZE=$(hyprctl getoption general:border_size -j | jq -r ".int")
+    local GAPS_OUT=$(
+        hyprctl getoption general:gaps_out -j |
+            jq -r ".custom" | cut -d ' ' -f 1
+    )
+    local BORDER_SIZE=$(
+        hyprctl getoption general:border_size -j |
+            jq -r ".int"
+    )
 
-    window_size=$(${window_size_map[$player]})
-    window_width=$(jq '.[0]' <<< "$window_size")
-    window_height=$(jq '.[1]' <<< "$window_size")
+    local window_size=$(${window_size_map[$player]})
+    local window_width=$(jq '.[0]' <<<"$window_size")
+    local window_height=$(jq '.[1]' <<<"$window_size")
 
-    x=$(( $MONITOR_WIDTH - $window_width - $GAPS_OUT - $BORDER_SIZE ))
-    y=$(( $MONITOR_HEIGHT - $window_height - $GAPS_OUT - $BORDER_SIZE ))
+    local x=$(($MONITOR_WIDTH - $window_width - $GAPS_OUT - $BORDER_SIZE))
+    local y=$(($MONITOR_HEIGHT - $window_height - $GAPS_OUT - $BORDER_SIZE))
 
-    hyprctl dispatch resizewindowpixel exact "$window_width $window_height", address:$address
-    hyprctl dispatch movewindowpixel exact "$x $y", address:$address
+    hyprctl dispatch resizewindowpixel exact $window_width $window_height, address:$address
+    hyprctl dispatch movewindowpixel exact $x $y, address:$address
     hyprctl dispatch pin address:$address
 }
 
@@ -48,17 +54,15 @@ unpin() {
 }
 
 active_player=$(
-  hyprctl activewindow -j |
-  jq -r '
-      select(
-          .class == "mpv" or
-          (.class == "firefox" and .title == "Picture-in-Picture")
-      )'
+    hyprctl activewindow -j | jq -r 'select(
+        .class == "mpv" or
+        (.class == "firefox" and .title == "Picture-in-Picture")
+    )'
 )
 
-address=$(jq -r ".address" <<< "$active_player")
-class=$(jq -r ".class" <<< "$active_player")
-
+class=$(jq -r ".class" <<<"$active_player")
 [[ ! "$class" =~ (mpv|firefox) ]] && exit
 
-jq -e ".pinned" <<< "$active_player" >/dev/null && unpin $address || pin $address $class
+address=$(jq -r ".address" <<< "$active_player")
+jq -e ".pinned" <<<"$active_player" >/dev/null &&
+    unpin $address || pin $address $class
