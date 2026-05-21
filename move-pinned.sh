9345231c64d279f879f71989c8c34eecfce8195c
hireki <hireki@proton.me>
2026-05-21 16:13:51 -0300

    [refactor]: split code into move_to_position and calculate_screen_edges functions

--- move-pinned.sh
+++ move-pinned.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-window=$(hyprctl -j clients | jq -c ".[] | select(.pinned == true)")
-[[ -z "$window" ]] && exit
+calculate_screen_edges() {
+    local MONITOR=$(hyprctl -j monitors | jq '.[]')
+    local MONITOR_WIDTH=$(jq '.width' <<< "$MONITOR")
+    local MONITOR_HEIGHT=$(jq '.height' <<< "$MONITOR")
+    local MONITOR_RESERVED=$(jq '.reserved[1]' <<< "$MONITOR")
 
-GAPS_OUT=$(hyprctl getoption general:gaps_out -j | jq -r ".custom" | cut -d ' ' -f 1)
-BORDER_SIZE=$(hyprctl getoption general:border_size -j | jq -r ".int")
+    local GAPS_OUT=$(
+        hyprctl getoption general:gaps_out -j |
+            jq -r '.custom' | cut -d ' ' -f 1
+    )
+    local BORDER_SIZE=$(
+        hyprctl getoption general:border_size -j |
+            jq '.int'
+    )
 
-MONITOR=$(hyprctl -j monitors | jq -c ".[]")
-MONITOR_WIDTH=$(jq ".width" <<< "$MONITOR")
-MONITOR_HEIGHT=$(jq ".height" <<< "$MONITOR")
-MONITOR_RESERVED=$(jq ".reserved[1]" <<< "$MONITOR")
+    local window_width=$(jq '.size[0]' <<< "$window")
+    local window_height=$(jq '.size[1]' <<< "$window")
 
-window_width=$(jq -c ".size[0]" <<< "$window")
-window_height=$(jq -c ".size[1]" <<< "$window")
-pos_x=$(jq -c ".at[0]" <<< "$window")
-pos_y=$(jq -c ".at[1]" <<< "$window")
+    local Y_MIN=$(($GAPS_OUT + $BORDER_SIZE + $MONITOR_RESERVED))
+    local Y_MAX=$(($MONITOR_HEIGHT - $window_height - $GAPS_OUT - $BORDER_SIZE))
+    local X_MIN=$(($GAPS_OUT + $BORDER_SIZE))
+    local X_MAX=$(($MONITOR_WIDTH - $window_width - $GAPS_OUT - $BORDER_SIZE))
 
-Y_MIN=$(( $GAPS_OUT + $BORDER_SIZE + $MONITOR_RESERVED ))
-Y_MAX=$(( $MONITOR_HEIGHT - $window_height - $GAPS_OUT - $BORDER_SIZE))
-X_MIN=$(( $GAPS_OUT + $BORDER_SIZE ))
-X_MAX=$(( $MONITOR_WIDTH - $window_width - $GAPS_OUT - $BORDER_SIZE ))
+    echo $Y_MIN $Y_MAX $X_MIN $X_MAX
+}
 
-declare -A actions=(
-    [up]="$pos_x $Y_MIN"
-    [down]="$pos_x $Y_MAX"
-    [left]="$X_MIN $pos_y"
-    [right]="$X_MAX $pos_y"
-)
-new_pos="${actions[$1]}" 
+move_to_position() {
+    local position="$1" # up down left right
+    local window="$2"
 
-address=$(jq -c ".address" <<< "$window" | sed 's/"//g') 
-hyprctl dispatch movewindowpixel exact ${new_pos[@]}, address:$address
+    local at_x=$(jq '.at[0]' <<< "$window")
+    local at_y=$(jq '.at[1]' <<< "$window")
+
+    local Y_MIN Y_MAX X_MIN X_MAX
+    read Y_MIN Y_MAX X_MIN X_MAX < <(calculate_screen_edges)
+
+    declare -A actions=(
+        [up]="$at_x $Y_MIN"
+        [down]="$at_x $Y_MAX"
+        [left]="$X_MIN $at_y"
+        [right]="$X_MAX $at_y"
+    )
+    local new_pos="${actions[$position]}"
+
+    local address=$(jq -r '.address' <<< "$window")
+
+    hyprctl dispatch movewindowpixel exact ${new_pos[@]}, address:$address
+}
+
+active_window=$(hyprctl -j activewindow)
+move_to_position "$1" "$active_window"
